a380cb9fa1b68191fe993b4fe2dc1df44825c0b3
David Conner <dconner.pro@gmail.com>
2013-07-03 21:51:29 -0600

    app/js/app.js: consolidated with router.js
    * run #config then #run, even though these are call backs
    
    * consolidating ensures that there are no load order problems
    
    * also makes dependency injection a little easier
    
    * app now works with ui-router, but still using $routeProvider

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,34 @@
-angular.module("app", []).run(function($rootScope) {
-  // adds some basic utilities to the $rootScope for debugging purposes
-  $rootScope.log = function(thing) {
-    console.log(thing);
-  };
 
-  $rootScope.alert = function(thing) {
-    alert(thing);
-  };
-});
+angular.module("app", ['ui.compat']).config(  
+  ['$stateProvider', '$routeProvider', '$urlRouterProvider',
+   function ($stateProvider,   $routeProvider,   $urlRouterProvider) {
+
+     $routeProvider.when('/login', {
+       templateUrl: 'angular/login.html',
+       controller: 'LoginController'
+     });
+
+     $routeProvider.when('/home', {
+       templateUrl: 'angular/home.html',
+       controller: 'HomeController'
+     });
+
+//     $routeProvider.otherwise({ redirectTo: '/login' });
+   }]).run(    
+     ['$rootScope', '$state', '$stateParams',
+      function ($rootScope,   $state,   $stateParams) {
+
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+
+        // adds some basic utilities to the $rootScope for debugging purposes
+        $rootScope.log = function(thing) {
+          console.log(thing);
+        };
+
+        $rootScope.alert = function(thing) {
+          alert(thing);
+        };
+
+      }]);
+
