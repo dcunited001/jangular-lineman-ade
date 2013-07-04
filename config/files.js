126953d3f91773f2f989042b12e63b0ca55e3c01
David Conner <dconner.pro@gmail.com>
2013-07-03 21:28:36 -0600

    config/files.js: specify load order for angular js/ui

--- config/files.js
+++ config/files.js
@@ -19,6 +19,8 @@ module.exports = require(process.env['LINEMAN_MAIN']).config.extend('files', {
   js: {
     vendor: [
       "vendor/js/angular.js",
+      "vendor/js/angular-ui-router.js",
+      "vendor/js/ui-bootstrap.js",
       "vendor/js/**/*.js"
     ],
     app: [
