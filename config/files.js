0901cf503ae8ad219b5b0f73dfe3518d55e93ef3
David Conner <dconner.pro@gmail.com>
2013-11-06 21:55:10 -0700

    lineman: added ui.router to files

--- config/files.js
+++ config/files.js
@@ -21,8 +21,8 @@ module.exports = require(process.env['LINEMAN_MAIN']).config.extend('files', {
       "bower_components/underscore/underscore.js",
       "bower_components/angular/angular.js",
       "bower_components/angular-route/angular-route.js",
-      "bower_components/angular-bootstrap/ui-bootstrap.js"
-      // "bower_components/angular-ui-router.js"
+      "bower_components/angular-bootstrap/ui-bootstrap.js",
+      "bower_components/angular-ui-router/release/angular-ui-router.js"
     ],
     app: [
       "app/js/app.js",
