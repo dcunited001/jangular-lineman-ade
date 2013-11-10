1186fcf871a8744f39152ff8ce997abb2ea0f69a
David Conner <dconner.pro@gmail.com>
2013-11-10 16:24:58 -0700

    Updated Authentication Service

--- app/js/services.js
+++ app/js/services.js
@@ -1,17 +1,30 @@
 app.factory('AuthenticationService', function($http, SessionService) {
+  'use strict';
+
   // these routes map to stubbed API endpoints in config/server.js
   return {
     login: function(credentials) {
+
+      // this method could be used to call the API and set the user 
+      //   instead of taking it in the function params
+      SessionService.currentUser = credentials.username;
       return $http.post('/login', credentials);
+
+      // TODO: fix login function (incorrect login page)
     },
+
     logout: function() {
       return $http.post('/logout');
+    },
+
+    isLoggedIn: function () {
+      return SessionService.currentUser !== null;
     }
   };
+
 });
 
 app.factory('SessionService', function () {
-
   'use strict';
 
   return {
