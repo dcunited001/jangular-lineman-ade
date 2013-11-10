bfb04fb468c313c20ff464c29560ecaa3aa5a053
David Conner <dconner.pro@gmail.com>
2013-11-09 18:40:46 -0700

    moved authentication service to services.js

---
+++ app/js/services.js
@@ -0,0 +1,11 @@
+app.factory('AuthenticationService', function($http) {
+  // these routes map to stubbed API endpoints in config/server.js
+  return {
+    login: function(credentials) {
+      return $http.post('/login', credentials);
+    },
+    logout: function() {
+      return $http.post('/logout');
+    }
+  };
+});
