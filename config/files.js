cde75a493906fd8f20604a162c8030aaf968f09d
David Conner <dconner.pro@gmail.com>
2013-11-09 18:36:01 -0700

    added bootstrap-responsive to lineman files

--- config/files.js
+++ config/files.js
@@ -35,6 +35,7 @@ module.exports = require(process.env['LINEMAN_MAIN']).config.extend('files', {
     app: [ "app/css/**/*.less" ],
     vendor: [ 
       "vendor/css/bootstrap.css", 
+      "vendor/css/bootstrap-responsive.css", 
       "vendor/css/**/*.less" 
     ]
 
