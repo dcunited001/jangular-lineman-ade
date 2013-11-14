4bc67948ba550384d2a9e3639fe18a8c51f19acd
David Conner <dconner.pro@gmail.com>
2013-11-13 23:25:51 -0700

    services: added RegistrationService and updated Auth Service

--- app/js/services.js
+++ app/js/services.js
@@ -1,20 +1,31 @@
+app.factory('RegistrationService', function($http) {
+  'use strict';
+  
+  return {
+    signup: function(params, success, error) {
+      return $http.post('/api/users.json', { user: params })
+        .then(success, error);
+    }
+  };
+});
+
 app.factory('AuthenticationService', function($http, SessionService) {
   'use strict';
 
   // these routes map to stubbed API endpoints in config/server.js
   return {
-    login: function(credentials) {
+    login: function(credentials, success, error) {
 
       // this method could be used to call the API and set the user 
       //   instead of taking it in the function params
       SessionService.currentUser = credentials.username;
-      return $http.post('/login', credentials);
-
-      // TODO: fix login function (incorrect login page)
+      
+      return $http.post('/api/', credentials)
+        .then(success, error);
     },
 
-    logout: function() {
-      return $http.post('/logout');
+    logout: function(success, error) {
+      return $http.post('/logout').then(cb);
     },
 
     isLoggedIn: function () {
